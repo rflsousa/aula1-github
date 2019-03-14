987d38cb17cf08bbdbf45804bdd6ead4497e0ce7
Rafael Sousa <rafael.oliveira.osv@gmail.com>
2019-03-14 16:13:12 -0300

    Mais palavras e retirada de espaços

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-
 		System.out.println("Boa tarde");
 		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 	}
 
 }
